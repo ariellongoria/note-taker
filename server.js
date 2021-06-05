431194919fa269f19eeed8377a384c44fdc3eda8
ariellongoria <ariello97@hotmail.com>
2021-06-04 21:45:54 -0500

    added endpoints for adding notes and landing page

--- server.js
+++ server.js
@@ -11,3 +11,44 @@
 // THEN that note appears in the right-hand column
 // WHEN I click on the Write icon in the navigation at the top of the page
 // THEN I am presented with empty fields to enter a new note title and the note’s text in the right-hand column
+
+const express = require("express");
+const path = require("path");
+const generateUniqueId = require("generate-unique-id");
+const addNote = require("./utils/addNote");
+const PORT = process.env.PORT || 3001;
+const notes = require("./db/db.json");
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "./public/index.html"));
+});
+
+app.get("/notes", (req, res) => {
+    res.sendFile(path.join(__dirname, "./public/notes.html"));
+});
+
+app.get("/api/notes", (req, res) => {
+    res.json(notes);
+});
+
+app.post("/api/notes", (req, res) => {
+    req.body.id = generateUniqueId({ length: 8 });
+    res.json(req.body);
+    addNote(req.body, notes);
+});
+
+app.delete("/api/notes", (req, res) => {});
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../../public/index.html"));
+});
+
+app.listen(PORT, () => {
+    console.log(`Server now live on port ${PORT}`);
+});
